5483ac194fb2ab26d5cdf9a5f9880eca723f05f3
depedro <depedro@secbit.io>
2018-08-27 21:34:53 +1000

    Moved the error reporting to variable decl visit.

--- secbit-tests/features/fixed-point/test.sol
+++ secbit-tests/features/fixed-point/test.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.4.23;
 
 contract ArrayContract {
-	function a() {
+	fixed a;
+	ufixed b;
+	ufixed128x18 c;
+	fixed128x18 d;
+	function f() {
 		fixed a;
 		ufixed b;
 		ufixed128x18 c;
